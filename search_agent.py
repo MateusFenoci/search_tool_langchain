a3bc3a7c9eba63603aff3ae5c11960c2e043baac
MateusFenoci <fenocimateus@gmail.com>
2024-10-01 20:27:26 -0300

    Agente responsável por pesquisar e resumir informação pela API da Wikipedia

---
+++ search_agent.py
@@ -0,0 +1,44 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+    return os.getenv("OPENAI_API_KEY")
+
+model = ChatOpenAI(
+    model = "gpt-3.5-turbo",
+    api_key = configure()
+)
+
+wikipedia_tool = WikipediaQueryRun(
+    api_wrapper=WikipediaAPIWrapper(
+        lang="pt"
+    )
+)
+
+agent_executor = create_python_agent(
+    llm = model,
+    tool = wikipedia_tool,
+    verbose = True,
+)
+
+prompt_template = PromptTemplate(
+    input_variables = ['query'],
+    template = '''
+        Pesquise na web sobre {query} e forneça um resumo sobre o assunto.
+        Responda tudo em portugues brasileiro.
+    '''
+)
+
+query = 'Alan Turing'
+
+prompt = prompt_template.format(query=query)
+
+response = agent_executor.invoke(prompt)
+
+print(response.get('output'))
